3800cb1ba220980f0edf732965f16c2349ed0351
Stas <sizovstanislav10@gmail.com>
2023-07-12 18:03:32 +0700

    m6 t1

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,30 @@
 package com.epam.mjc.stage0;
 
+
 public class Animal {
+
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
+
+    public Animal(String color, int numberOfPaws, boolean hasFur){
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+
+      public String getDescription(){
+            String paws = "paw";
+            String fur = "no";
+        String Paws = Integer.toString(numberOfPaws);
+        if(numberOfPaws > 1) paws = "paws";
+        if(hasFur) fur = "a";
+       String result = "This animal is mostly " + color + ". " + "It has " + Paws +" "+paws + " and " + fur +" fur.";
+       return result;
+    }
+
+
+
 
 }
